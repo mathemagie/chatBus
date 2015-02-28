c512bcec68b3aa81b4075a63cd42750fe66496cc
Fache Aurélien <aurelien.fache@gmail.com>
2015-02-28 16:15:51 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -4,21 +4,23 @@
 #include <Servo.h>
 
 Servo monServo;
-const int ledPin = 6; 
+const int ledPin = 13; 
 
 void setup() {
-  Bridge.begin();   // Initialize the Bridge
-  Console.begin(); 
+  
   monServo.attach(9); 
   pinMode(ledPin, OUTPUT);
-   monServo.write(0);
- 
+  digitalWrite(ledPin, LOW);
+  Bridge.begin();   // Initialize the Bridge
+  digitalWrite(ledPin, HIGH);
+  Console.begin(); 
+  monServo.write(0);
 }
 
 void loop() {
   Process p;
   p.runShellCommand("/usr/bin/curl http://mathemagie.net/projects/arret_demande/get_data_bus.php");
-  Console.println("run curl command");
+  Console.println("run curl OK");
   
   while(p.running());  
 
@@ -26,37 +28,28 @@ void loop() {
   while (p.available()) {
     int result = p.parseInt();          // look for an integer
     if (result == 1) {
-      Console.println("light on");
-      Serial.println("light on");
-      digitalWrite(ledPin, HIGH);
+     
+     Serial.println("bus is here");
+     digitalWrite(ledPin, HIGH);
      for (int i=0; i <= 100; i++){
           monServo.write(i);
-          Serial.println("a" + String(i));
-        delay(2);
+          delay(2);
      } 
-      //delay(250);
-      //digitalWrite(ledPin, LOW);
-     
-      //delay(250);
-      //digitalWrite(ledPin, HIGH);
-       //monServo.write(0);
-       //delay(250);
-     
-      for (int i=100; i >= 1; i--){
+     for (int i=100; i >= 1; i--){
           monServo.write(i);
-          Serial.println("b" + String(i));
-        delay(2);
+         delay(2);
      }
      digitalWrite(ledPin, LOW); 
     }
+    
      if (result == 2) {
         digitalWrite(ledPin, HIGH);//bug in parsing process
      }
        
     if (result == 0) {
         digitalWrite(ledPin, LOW);//bug in parsing process
-        Console.println("light off");
-         Serial.println("light OFF");
+        Console.println("bus not here");
+        Serial.println("bus not here");
     }
   
     
